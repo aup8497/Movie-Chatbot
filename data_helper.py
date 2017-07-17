1b8c13853f5ada35693f13da576b3c78928f089b
aup8497 <akshayup609@gmail.com>
2017-07-17 21:56:37 +0530

    updated data_helper.py

--- data_helper.py
+++ data_helper.py
@@ -1,4 +1,6 @@
 import random
+import pickle
+import nltk
 
 ''' 
     1. Read from 'movie-lines.txt'
@@ -47,9 +49,9 @@ def extract_conversations(convs,id2line,path=''):
 '''
 def gather_dataset(convs, id2line):
     questions = []; answers = []
-
+    # here questions are encoding data to be sent to the seqseq model
     for conv in convs:
-        if len(conv) %2 != 0:
+        if len(conv) %2 != 0:   # if the conversation has odd number of terms , then remove the last one so that we can assign odd sentences as questions and even sentences as answer
             conv = conv[:-1]
         for i in range(len(conv)):
             if i%2 == 0:
@@ -93,19 +95,80 @@ def prepare_seq2seq_files(questions, answers, path='',TESTSET_SIZE = 30000):
     train_dec.close()
     test_enc.close()
     test_dec.close()
-            
+    
 
-####
-# main()
-####
+def create_data_set_and_load_into_pickle():
+    id2line = get_id2line()
+    #  here id2line = {  'L487031': "Just relax. You're at school now. No one can get you here.", 'L487030': 'Not this time. I owe Cotton that much. Hell, even I thought that man was guilty.', 'L487037': "This is a mistake. I shouldn't be here." }
+    print( '>> gathered id2line dictionary.\n')
+    convs = get_conversations()
+    # here convs = [ ['L665932', 'L665933'], ['L665936', 'L665937'], ['L665938', 'L665939']]
+    print ('>> gathered conversations.\n')
+    questions, answers = gather_dataset(convs,id2line) # here questions and answers are every two consecutive sentences in the big dataset of conversations
+    # here questios and answers are in the form => 'Can we make this quick?.'
+
+    # storing these sentences in a pickle
+
+    data ={}
+    data['questions'] = questions
+    data['answers'] = answers
+
+    print ('>> writing questions and answers data into data.pkl\n')
+    with open('data.pkl','wb') as fp:
+        pickle.dump(data,fp)
+
+    # you can load the dict using 
+    # with open("test.txt", "rb") as fp:   # Unpickling
+    #   b = pickle.load(fp)
+
+
+def getID(word, vocab, create=True):
+    word = word.lower()
+    wid = vocab["word2id"].get(word,vocab, -1)
+    if wid == -1:
+        if create:
+            wid = len(vocab["word2id"])
+            vocab["word2id"][word] = wid
+        else:
+            wid = vocab["word2id"].get("<unknown>")
+    return wid
+
+
+def generate_vocab():
+
+    with open("data.pkl", "rb") as fp:   # Unpickling
+        data = pickle.load(fp)
+
+
+    print('Creating vocabulary...')
+    vocab = {}
+    vocab["word2id"] = {}
+    vocab["id2word"] = {}
+
+    getID('<go>',vocab)
+    getID('<pad>',vocab)
+    getID('<eos>',vocab)
+    getID('<unknown>',vocab)
+
+    for sentence in data['questions']:
+        for word in nltk.word_tokenize(sentence.decode('ISO-8859-1')):
+            getID(word,vocab)
+
+    vocab["id2word"] = { v: k for k, v in vocab["word2id"].items() }
+    print('Vocabulary created.Inverse vocabulary also created')
+    print("Created vocabulary of " + str(len(vocab["word2id"])) + " words.")
+
+    print ('>> writing vocab data into vocab.pkl\n')
+    with open('vocab.pkl','wb') as fp:
+        pickle.dump(vocab,fp)
+
+
+def sen2enco(sentence):  #ak - this encodes the sentence into list of word indices
+  return [getID(word, create=False) for word in nltk.word_tokenize(sentence)[:MAX_INPUT_LENGTH]]  
+
+
 if __name__ == '__main__':
-	id2line = get_id2line()
-	print( '>> gathered id2line dictionary.\n')
-	convs = get_conversations()
-	print ('>> gathered conversations.\n')
-	questions, answers = gather_dataset(convs,id2line)
-	print( questions[30:34])
-	print(answers[30:34])
-	#print '>> gathered questions and answers.\n'
-	prepare_seq2seq_files(questions,answers)
-	
+    # if sys.argv[1] == '--create_data':
+        create_data_set_and_load_into_pickle()
+    # elif sys.argv[1] == '--generate_vocab':
+        generate_vocab()    
